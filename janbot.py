b1698960d281d40584ec25dfee1eee8d7ce2be61
kevinhu98 <kh2547@nyu.edu>
2021-01-24 15:09:48 -0500

    added new function for exalt+chaos equivalent and spacing issues

--- janbot.py
+++ janbot.py
@@ -27,6 +27,7 @@ with open("interviewquestions.txt") as f:
 for path in richardPicDir:
     richardPics.append(path.replace("\\", "/"))
 
+
 @bot.command(name="exalt")
 async def exalt(ctx):
     request_string = 'https://poe.ninja/api/data/CurrencyOverview?league=Ritual&type=Currency&language=en'
@@ -36,7 +37,8 @@ async def exalt(ctx):
             returnStatement = currency['currencyTypeName'] +'s' + ' are currently worth ' + str(currency['chaosEquivalent']) + " chaos orbs."
     await ctx.send(returnStatement)
 
-@bot.command(aliases=["c","chaos"])
+
+@bot.command(name="c", aliases=["chaos"])
 async def chaosEquivalent(ctx, *args):
     requested_currency = " ".join(args)
     request_string = 'https://poe.ninja/api/data/CurrencyOverview?league=Ritual&type=Currency&language=en'
@@ -53,7 +55,8 @@ async def chaosEquivalent(ctx, *args):
         return_statement = "Cannot find: " + requested_currency + ". \n" + "Please make sure that you are typing the full name of the currency."
         await ctx.send(return_statement)
 
-@bot.command(aliases=["e"])
+
+@bot.command(name="e")
 async def exaltEquivalent(ctx, *args):
     requested_currency = " ".join(args)
     request_string = 'https://poe.ninja/api/data/CurrencyOverview?league=Ritual&type=Currency&language=en'
@@ -64,7 +67,7 @@ async def exaltEquivalent(ctx, *args):
             exalt_value = currency['chaosEquivalent']
             break
 
-    currency_dict = {}  # dict format -- name: [correct name: chaos equivalent]
+    currency_dict = {}  # dict format -- name: [correct name: exalt equivalent]
     for currency in r.json()['lines']:
         simplified_name = currency['currencyTypeName'].replace("'", "").lower()  # remove apostrophe and lowercase
         currency_dict[simplified_name] = [currency['currencyTypeName'], str(round(currency['chaosEquivalent'] / exalt_value, 5))]
@@ -72,9 +75,35 @@ async def exaltEquivalent(ctx, *args):
     try:
         return_statement = currency_dict[requested_currency][0] + '\'s' + " are worth " + "**" + currency_dict[requested_currency][1] + "**" + " exalted orbs."
         await ctx.send(return_statement)
-    except:
+    except:  # make less broad
         return_statement = "Cannot find: " + requested_currency + ". \n" + "Please make sure that you are typing the full name of the currency."
         await ctx.send(return_statement)
+
+
+@bot.command(name="ec", aliases=["ce"])
+async def exaltChaosEquivalent(ctx, *args):
+    requested_currency = " ".join(args)
+    request_string = 'https://poe.ninja/api/data/CurrencyOverview?league=Ritual&type=Currency&language=en'
+    r = requests.get(request_string)
+
+    for currency in r.json()['lines']:
+        if currency['currencyTypeName'] == "Exalted Orb":
+            exalt_value = currency['chaosEquivalent']
+            break
+
+    currency_dict = {}  # dict format -- name: [correct name, exalt quotient, chaos remainder]
+    for currency in r.json()['lines']:
+        simplified_name = currency['currencyTypeName'].replace("'", "").lower()  # remove apostrophe and lowercase
+        currency_dict[simplified_name] = [currency['currencyTypeName'], str(currency['chaosEquivalent'] // exalt_value), str(round(currency['chaosEquivalent'] % exalt_value))]
+        currency_dict[currency['currencyTypeName']] = [currency['currencyTypeName'], str(currency['chaosEquivalent'] // exalt_value), str(round(currency['chaosEquivalent'] % exalt_value))]
+    try:
+        return_statement = currency_dict[requested_currency][0] + '\'s' + " are worth a total of " + \
+                           "**" + currency_dict[requested_currency][1] + "**" + " exalted orbs and " + currency_dict[requested_currency][2] + " chaos orbs."
+        await ctx.send(return_statement)
+    except:  # make less broad
+        return_statement = "Cannot find: " + requested_currency + ". \n" + "Please make sure that you are typing the full name of the currency."
+        await ctx.send(return_statement)
+
 
 @bot.command(name="bf")  # bottled faith
 async def bf(ctx):
@@ -95,25 +124,31 @@ async def currency(ctx):
         await ctx.send(returnStatement)
 '''
 
+
 @bot.command(name="hello")
 async def hello(ctx):
     await ctx.send("im janbot")
+
 
 @bot.command(name="choke")
 async def choke(ctx):
     await ctx.send(file=discord.File('richardpics/richardchoke.jpg'))
 
+
 @bot.command(name="whoisright")
 async def whoisright(ctx):
     await ctx.send(file=discord.File('richardpics/fiddle.jpg'))
+
 
 @bot.command(name="richardree")
 async def richardree(ctx):
     await ctx.send(file=discord.File('richardpics/richardgasm.jpg'))
 
+
 @bot.command(name="swagswagbitch")
 async def richardree(ctx):
     await ctx.send(file=discord.File('richardpics/richard9.jpg'))
+
 
 @bot.command(name='gunmo')
 async def gunmo(ctx):
@@ -122,33 +157,41 @@ async def gunmo(ctx):
 
     await ctx.send(response)
 
+
 @bot.command(name="!richard")
 async def randomRichard(ctx):
     await ctx.send(file=discord.File(random.choice(richardPics)))
+
 
 @bot.command(name="poggers")
 async def ryanO(ctx):
     await ctx.send(file=discord.File('images/ryanO.png'))
 
+
 @bot.command(name="!jimothy")
 async def jimothy(ctx):
     await ctx.send(file=discord.File('images/jimothy.png'))
+
 
 @bot.command(name="!burger")
 async def burger(ctx):
     await ctx.send(file=discord.File('images/burger.jpg'))
 
+
 @bot.command(name="sike")
 async def sike(ctx):
     await ctx.send(file=discord.File('images/sike.png'))
+
 
 @bot.command(name="!question")
 async def interviewquestion(ctx):
     await ctx.send(random.choice(interviewQuestions))
 
+
 @bot.command(name="!stonks")
 async def stonks(ctx):  # portfolio
     await ctx.send(robin_stocks.profiles.load_portfolio_profile(info="equity"))
+
 
 @bot.command(name="!positions")
 async def positions(ctx):  # portfolio
